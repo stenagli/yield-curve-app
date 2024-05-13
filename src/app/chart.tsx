0ad5c4960fdece6cec8d247f8b99df4c94a7a798
Steven Tenaglia <steven.a.tenaglia@gmail.com>
2024-05-13 13:44:13 -0400

    Working chart

---
+++ src/app/chart.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+import { Line } from 'react-chartjs-2';
+
+export default function YieldChart({ yield_data }: { yield_data: string[] }) {
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const data = {
+    labels: yield_data[0].split(',').slice(1, -1),
+    datasets: [{
+      data: yield_data[1].split(',').slice(1, -1),
+      borderColor: 'rgb(75, 192, 192)'
+    }]
+  };
+
+  return (
+    <Line data={data} />
+  );
+}
