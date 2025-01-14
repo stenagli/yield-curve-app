6586912e669e3b16e351ca43b444f381aac40494
stenagli <steven.a.tenaglia@gmail.com>
2025-01-13 21:08:44 -0500

    Correctly use Suspense when loading chart data (#16)

--- src/app/nominal-yield-chart.tsx
+++ src/app/nominal-yield-chart.tsx
@@ -4,11 +4,9 @@ import YieldChart from './chart';
 import { fetchNominalData } from './utils';
 
 export default async function RealYieldChart() {
-  const nominal_data = await fetchNominalData();
-
   return (
     <Suspense fallback={<Loading />}>
-      <YieldChart yield_csv={nominal_data} title={'Nominal Yields'} borderColor={'rgb(255, 87, 51)'} />
+      <YieldChart yieldCsvPromise={fetchNominalData()} title={'Nominal Yields'} borderColor={'rgb(255, 87, 51)'} />
     </Suspense>
   )
 };
--- src/app/real-yield-chart.tsx
+++ src/app/real-yield-chart.tsx
@@ -4,11 +4,9 @@ import YieldChart from './chart';
 import { fetchRealData } from './utils';
 
 export default async function RealYieldChart() {
-  const real_data = await fetchRealData();
-
   return (
     <Suspense fallback={<Loading />}>
-      <YieldChart yield_csv={real_data} title={'Real Yields'} borderColor={'rgb(75, 192, 192)'} />
+      <YieldChart yieldCsvPromise={fetchRealData()} title={'Real Yields'} borderColor={'rgb(75, 192, 192)'} />
     </Suspense>
   )
 };
