d8c86f0673fe580dc7a0536a5301d6e8b501eed6
Steven Tenaglia <steven.a.tenaglia@gmail.com>
2025-01-13 19:16:20 -0500

    Use last month's data on the first of the month

--- src/app/utils.tsx
+++ src/app/utils.tsx
@@ -7,12 +7,46 @@ export async function fetchNominalData(): Promise<string> {
 }
 
 async function fetchData(type: string): Promise<string> {
-  return (await fetch(`https://home.treasury.gov/resource-center/data-chart-center/interest-rates/daily-treasury-rates.csv/all/${yearMonth()}?type=${type}&field_tdr_date_value_month=${yearMonth()}&page&_format=csv`, { cache: 'no-store' })).text()
+  let data = await fetchDataToday(type);
+  if (!data && day() === 1) {
+    // This month's data has not been reported yet
+    // so display the most recent data from last month
+    data = await fetchDataYesterday(type);
+  }
+  return data;
 }
 
-function yearMonth(): string {
+function fetchDataToday(type: string) {
+  return fetchDataTemplate({ yearMonth: yearMonthToday(), type });
+}
+
+function fetchDataYesterday(type: string) {
+  return fetchDataTemplate({ yearMonth: yearMonthYesterday(), type });
+}
+
+async function fetchDataTemplate({ yearMonth, type }: {
+  yearMonth: string;
+  type: string
+}) {
+return (await fetch(`https://home.treasury.gov/resource-center/data-chart-center/interest-rates/daily-treasury-rates.csv/all/${yearMonth}?type=${type}&field_tdr_date_value_month=${yearMonth}&page&_format=csv`, { cache: 'no-store' })).text()
+}
+
+const day = () => new Date().getDate();
+
+function yearMonthToday() {
   const today = new Date();
-  const year = today.getFullYear();
-  const month = ('0' + (today.getMonth()+1)).slice(-2); // e.g. '05' for May
-  return year + month;
+  return yearMonth(today)
+}
+
+
+function yearMonthYesterday() {
+  const yesterday = new Date(Date.now() - 24 * 60 * 60 * 1000);
+  return yearMonth(yesterday);
+}
+
+function yearMonth(date: Date): string {
+  const year = date.getFullYear();
+  const month = date.getMonth()+1;
+  const paddedMonth = ('0' + month).slice(-2); // e.g. '05' for May
+  return year + paddedMonth;
 }
